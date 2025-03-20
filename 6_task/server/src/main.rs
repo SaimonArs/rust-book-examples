f8c5bb9c72624c1eab399b5c298f388fb8a5c572
SaimonArs <ares_sai_2004@mail.ru>
2025-03-21 02:50:55 +0300

    fix database initialization

--- 6_task/server/src/main.rs
+++ 6_task/server/src/main.rs
@@ -5,10 +5,12 @@ use server::{db_client::*, listener::*};
 fn main() {
 
     let postgres_url = env::var("DATABASE_URL").unwrap();
+    
     let addr = env::var("ADDRESS").unwrap();
     let port = env::var("PORT").unwrap();
 
-    let db = Database::build(postgres_url);
+    let mut db = Database::build(postgres_url);
+    db.init_table();
     let mut listener = Listener::build(db, addr, port);
 
     listener.start();
